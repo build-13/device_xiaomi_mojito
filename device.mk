9445bfbfadc0186926830effb692fc9328cf8155
David Sehr <sehr@google.com>
2020-04-25 15:50:40 -0700

    mojito: Enable usage of dex2oat64
    * Use 64-bit dex2oat for better dexopt time.
    
    Bug: 153380900
    Test: boot and install an application
    Change-Id: I3e7a6e6e9385ff6564d1a2e6dda004ebb061f095
    (cherry picked from commit 126f03be80f57a8a0411842011152d9381589b78)
    Merged-In: I3e7a6e6e9385ff6564d1a2e6dda004ebb061f095
    Signed-off-by: Rve27 <rve27github@gmail.com>
    Signed-off-by: build-13 <173170056+build-13@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -173,7 +173,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.consumerir.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.consumerir.xml
 
-# Device-specific settings
+# Dex
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+PRODUCT_SYSTEM_PROPERTIES += \
+    dalvik.vm.dex2oat64.enabled=true
+
+# Device Specific Settings
 PRODUCT_PACKAGES += \
     XiaomiParts
 
