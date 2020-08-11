770efa3c922f4512c573b4662d67a60e84750469
kazerine <kamikaze.mrinal@gmail.com>
2020-08-12 00:18:36 +0530

    hii arrow

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/derp_mojito.mk
+    $(LOCAL_DIR)/arrow_mojito.mk
 
 COMMON_LUNCH_CHOICES := \
-    derp_mojito-user \
-    derp_mojito-userdebug \
-    derp_mojito-eng
+    arrow_mojito-user \
+    arrow_mojito-userdebug \
+    arrow_mojito-eng
