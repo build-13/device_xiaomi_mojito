12503f102636dc1ad9737589c0f93613940142d0
pyo3377 <75950219+pyo3377@users.noreply.github.com>
2024-11-16 16:18:46 +0700

    mojito : enable CLO Framework Boost

--- device.mk
+++ device.mk
@@ -366,6 +366,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 PRODUCT_PACKAGES += \
     MojitoAperture \
     MojitoCarrierConfig \
